73472fb4fd3ecd809f94658892a79e8a4a17f78d
ChaymaeMharzi <102840486+ChaymaeMharzi@users.noreply.github.com>
2023-05-11 18:29:04 +0100

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+
+   firebaseConfig: {
+    apiKey: "AIzaSyCs1_iVLk34n4OLHUGJw5QCyvm2Sjgnpas",
+    authDomain: "io-db-v2.firebaseapp.com",
+    projectId: "io-db-v2",
+    storageBucket: "io-db-v2.appspot.com",
+    messagingSenderId: "1021576205462",
+    appId: "1:1021576205462:web:deaf5c7e90f1bb15449eb7",
+    measurementId: "G-363G4MEB45"
+  }
+};
